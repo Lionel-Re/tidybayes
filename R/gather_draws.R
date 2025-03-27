1e886160ae49f0ee18b349b5194b18b6bfd92725
KANHONOU Lionel <id2148@clust-n1.domensai.ecole>
2025-03-27 11:53:54 +0100

    fix: unnest NA issues (#330) and (#332)

--- R/gather_draws.R
+++ R/gather_draws.R
@@ -11,34 +11,40 @@
 #' @importFrom rlang enquos
 #' @export
 gather_draws = function(
-  model,
-  ...,
-  regex = FALSE,
-  sep = "[, ]",
-  ndraws = NULL,
-  seed = NULL,
-  draw_indices = c(".chain", ".iteration", ".draw"),
-  n
+    model,
+    ...,
+    regex = FALSE,
+    sep = "[, ]",
+    ndraws = NULL,
+    seed = NULL,
+    draw_indices = c(".chain", ".iteration", ".draw"),
+    n
 ) {
   ndraws = .Deprecated_argument_alias(ndraws, n)
-
+  
   draws = sample_draws_from_model_(model, ndraws, seed)
-
+  
   draw_indices = intersect(draw_indices, names(draws))
   tidysamples = lapply(enquos(...), function(variable_spec) {
-    gather_variables(
+    tidied = gather_variables(
       spread_draws_(draws, variable_spec, regex = regex, sep = sep, draw_indices = draw_indices),
       exclude = c(draw_indices, ".row")
     )
+    
+    # Drop NAs for the specific variable columns after gathering
+    tidied = tidied %>%
+      filter(!is.na(.value))  # Filter out rows where .value is NA
+    
+    return(tidied)
   })
-
+  
   #get the groups from all the samples --- when we bind them together,
   #the grouping information is not always retained, so we'll have to recreate
   #the full set of groups from all the data frames after we bind them
   groups_ = tidysamples %>%
     lapply(group_vars) %>%
     reduce_(union)
-
+  
   bind_rows(tidysamples) %>%
     group_by_at(groups_)
 }
